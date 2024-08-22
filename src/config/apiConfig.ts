aab9d6af28e54819ae3ede295393dadfa5c66d3a
Marcos Lima <marcos.lima@fiap.com.br>
2024-08-22 19:32:29 -0300

    ajustes de menus

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL= "https://api-makebe2.ddns.net/"
+// export const API_BASE_URL= "https://localhost:44368/"
 export const URL_IMAGENS = "https://makebe2.ddns.net/imagens";
 export const RECAPTCHA_SITE_KEY = "sua-site-key-aqui";
