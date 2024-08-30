ab6d63b476065263462dff4ff64f65b158aa16ed
Marcos Lima <marcos.lima@fiap.com.br>
2024-08-30 18:24:50 -0300

    verificando sessao em prod

--- src/containers/Home/home.tsx
+++ src/containers/Home/home.tsx
@@ -15,19 +15,17 @@ const Home: React.FC = () => {
   const [useVritrine, setVitrine] = useState<VitrineItem>();
   const [useUsuarioLogado, setUsuarioLogado] = useState<UsuarioLogadoItens>();
 
-  (async () => {
-      const sessao = await UsuarioLogadoService();
-      setUsuarioLogado(sessao);
-  })();
 
   const fetchVitrineData = async () => {
       const data = await VitrineService();
       setVitrine(data); 
+      const sessao = await UsuarioLogadoService();
+      setUsuarioLogado(sessao);
   };
    useEffect(() => {
         fetchVitrineData();
     }, []);
-console.log(useUsuarioLogado);
+
 
   return (
     <>
