8ad59d532a2a6535fbae2e8980f6920d32b78c87
marcos <marcoslima237@outlook.com>
2025-12-16 18:00:48 -0300

    Update apiConfig.ts

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,8 +1,8 @@
 // export const API_BASE_URL= "https://api-makebe2.ddns.net/"
 // export const API_BASE_AGENDA_URL= "https://api-agenda-makebe2.ddns.net/"
 //export const API_BASE_URL= "https://localhost:44368/"
-export const API_BASE_URL= "http://www.makebe2.com/api/makebe-session/"
-export const API_BASE_AGENDA_URL= "https://localhost:44330/"
+export const API_BASE_URL= "https://www.makebeapp.com.br/api/makebe-session/"
+export const API_BASE_AGENDA_URL= "https://www.makebeapp.com.br/api/Agenda"
 
 export const URL_IMAGENS = "https://www.makebeapp.com.br/imagens";
 export const RECAPTCHA_SITE_KEY = "6Lcb1i0sAAAAADPx6Nbn6e5c6PNfIq8QvYP-XCm3";
