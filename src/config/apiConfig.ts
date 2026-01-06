65fcff85e1e048c26abd8f970439ceb3829e764c
marcos <marcoslima237@outlook.com>
2026-01-06 18:00:02 -0300

    Update apiConfig.ts

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -5,4 +5,4 @@ export const API_BASE_URL= "https://www.makebeapp.com.br/api/makebe-session/"
 export const API_BASE_AGENDA_URL= "https://www.makebeapp.com.br/api/Agenda"
 
 export const URL_IMAGENS = "https://www.makebeapp.com.br/imagens";
-export const RECAPTCHA_SITE_KEY = "6LdHZUIsAAAAAKGfscDqt2gweZt2JlxDH23srQl1";
+export const RECAPTCHA_SITE_KEY = "6LeQZ0IsAAAAALpkPpap5_FVYux0KcPG1wVUnEYk";
