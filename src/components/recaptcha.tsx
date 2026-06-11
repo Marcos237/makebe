265c26d56f7b00f8943b0e9a2fa7f74d8666997d
Marcos Lima <marcos.lima@fiap.com.br>
2026-06-11 20:47:17 -0300

    ajustes de novo layout

--- src/components/recaptcha.tsx
+++ src/components/recaptcha.tsx
@@ -2,20 +2,10 @@ import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { RecaptchaItens } from "../Interfaces/shared/RecaptchaItens";
 
-
-
-const RecaptchaComponent: React.FC<RecaptchaItens> = ({ siteKey, onChange }) => {
-
-  if (window.grecaptcha && window.grecaptcha.enterprise) {
-    window.grecaptcha.enterprise.ready(async () => {
-
-      const token = await window.grecaptcha.enterprise.execute(siteKey, { action: 'submit' });
-      onChange(token);
-
-    });
-  };
-
-
+const RecaptchaComponent: React.FC<RecaptchaItens> = ({
+  siteKey,
+  onChange,
+}) => {
   return (
     <ReCAPTCHA
       sitekey={siteKey}
