9dd55b54eeb9f608775ea731187b885cc229698c
Marcos Lima <marcos.lima@fiap.com.br>
2026-06-19 17:29:12 -0300

    nova chave recaptcha makebeapp

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -2,6 +2,4 @@ export const API_BASE_URL= "https://www.makebeapp.com.br/api/makebe-session/"
 export const API_BASE_AGENDA_URL= "https://www.makebeapp.com.br/api/makebe-agenda/"
 
 export const URL_IMAGENS = "https://www.makebeapp.com.br/imagens";
-export const RECAPTCHA_SITE_KEY =
-    process.env.REACT_APP_RECAPTCHA_SITE_KEY ||
-    "6Lfw3ygtAAAAAK98vwDpSIArKdj8EOCJiXUu5nyj";
+export const RECAPTCHA_SITE_KEY = "6Lfw3ygtAAAAAK98vwDpSIArKdj8EOCJiXUu5nyj";
