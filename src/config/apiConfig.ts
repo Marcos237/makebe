81ec676e024d6c8c3129ac1414ebe4b05756468f
Marcos Lima <marcos.lima@fiap.com.br>
2026-06-22 19:23:01 -0300

    ajustes de notificacoes

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,8 +1,8 @@
-// export const API_BASE_URL= "https://www.makebeapp.com.br/api/makebe-session/"
-// export const API_BASE_AGENDA_URL= "https://www.makebeapp.com.br/api/makebe-agenda/"
+export const API_BASE_URL= "https://www.makebeapp.com.br/api/makebe-session/"
+export const API_BASE_AGENDA_URL= "https://www.makebeapp.com.br/api/makebe-agenda/"
 
-export const API_BASE_URL= "https://makebeappdev/api/makebe-session/"
-export const API_BASE_AGENDA_URL= "https://makebeappdev/api/makebe-agenda/"
+// export const API_BASE_URL= "https://makebeappdev/api/makebe-session/"
+// export const API_BASE_AGENDA_URL= "https://makebeappdev/api/makebe-agenda/"
 
 export const URL_IMAGENS = "https://www.makebeapp.com.br/imagens";
 export const RECAPTCHA_SITE_KEY = "6LfX6CgtAAAAAE-uz_RXIwqdu4YdIrt2GzXGaITg";
