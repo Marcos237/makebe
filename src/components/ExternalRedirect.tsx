30c8b55baf8b3886af210413068f8364f9472212
Marcos Lima <marcos.lima@fiap.com.br>
2026-06-22 19:56:20 -0300

    ajustes de notificacoes

--- src/components/ExternalRedirect.tsx
+++ src/components/ExternalRedirect.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from 'react';
 import { buscarUsuarioLogadoHome } from '../features/home/services/homeService';
 
+const VITRINE_URL = 'https://www.makebeapp.com.br/vitrine/';
+
 const ExternalRedirect: React.FC = () => {
   useEffect(() => {
     const redirectToInitialUrl = async () => {
       const sessao = await buscarUsuarioLogadoHome();
       const usuarioLogado = sessao?.data;
-      const targetUrl =
-        usuarioLogado?.urlInicial ||
-        usuarioLogado?.menus?.find(menu => menu.menuDescricao?.toLowerCase() === 'home')?.menuUrl ||
-        '/vitrine';
+      const hasSession = Boolean(usuarioLogado);
+      const targetUrl = hasSession
+        ? (
+            usuarioLogado?.urlInicial ||
+            usuarioLogado?.menus?.find(menu => menu.menuDescricao?.toLowerCase() === 'home')?.menuUrl ||
+            VITRINE_URL
+          )
+        : VITRINE_URL;
 
       window.location.href = targetUrl;
     };
